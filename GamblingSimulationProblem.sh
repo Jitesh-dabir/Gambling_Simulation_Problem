3f132f7162ec4b95b203ef42bc4c577f6ff6a24d
Jitesh-Dabir <jitudabir9@gmail.com>
2020-02-20 17:36:55 +0530

    ADDED:After twenty days of playing every day would like to know total amount won or lost

--- GamblingSimulationProblem.sh
+++ GamblingSimulationProblem.sh
@@ -1,23 +1,27 @@
-#!/bin/bash -x
+#!/bin/bash -X 
 
 echo ".............................. Gambling Simulation Problem .............................."
+
+#DECLARE A DICTIONARY
+declare -A dailyCash
 
 #CONSTANT
 STAKE=100
 BET=1
 IS_WIN=1
+NUMBER_OF_DAYS=20
 MAXWIN=$(($STAKE+$STAKE/2))
 MAXLOOSE=$(($STAKE-$STAKE/2))
 
 #VARIABLE
 cash=0
-
-#GENERATE RANDOM NUMBER FOR GAMBLER
-randomNumber=$((RANDOM%2))
+limit=0
 
 #FUNCTION TO GET DAILY CASH WIN OR LOOSE
 function gamble()
 {
+	#GENERATE RANDOM NUMBER FOR GAMBLER
+   randomNumber=$((RANDOM%2))
 	cash=$STAKE 
 	while [[ $cash -ne $MAXWIN && $cash -ne $MAXLOOSE ]]
 	do
@@ -28,8 +32,21 @@ function gamble()
 			cash=$(($cash-$BET))
 		fi
 	done
-	echo "Cash:"$cash
+	echo "$cash"
 }
 
-#FUNCTION CALL TO GET DAILY CASH
-gamble
+#CHECK IF WIN OR LOOSE FOR 20 DAYS AND FIND TOTAL AMOUNT 
+for (( day=1; day<=$NUMBER_OF_DAYS; day++))
+do
+	cashPerDay=$(gamble)
+	cashPerDay=$(( $cashPerDay - $STAKE ))
+	dailyCash[Day $day]=$cashPerDay 
+	totalCash=$(($totalCash+$(($cashPerDay))))
+	if [ $cashPerDay -gt 0 ]
+	then
+		echo "Day$day  win   :"$cashPerDay"$"
+	else
+		echo "Day$day  loose:"$cashPerDay"$"
+	fi
+done
+echo "Total cash for 20 days:$totalCash"
