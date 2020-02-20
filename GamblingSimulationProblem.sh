6fea1527808dec54689d7116627e65fc92df76d1
Jitesh-Dabir <jitudabir9@gmail.com>
2020-02-20 16:40:28 +0530

    ADDED:Gambler make  bet so either win  or loose

--- GamblingSimulationProblem.sh
+++ GamblingSimulationProblem.sh
@@ -1,7 +1,29 @@
 #!/bin/bash -x
 
+#DISPLAY WELCOME MESSAGE
 echo ".............................. Gambling Simulation Problem .............................."
 
 #CONSTANT
 STAKE=100
 BET=1
+IS_WIN=1
+
+#VARIABLE
+cash=0
+
+#GENERATE RANDOM NUMBER FOR GAMBLER
+randomNumber=$((RANDOM%2))
+
+#CHECK IF WIN OR LOSE
+cash=$STAKE
+if [ $randomNumber -eq $IS_WIN ]
+then
+	echo "Win :"$IS_WIN"$"
+	cash=$(($cash+$BET))
+else
+	echo "Lose:"$IS_WIN"$"
+	cash=$(($cash-$BET))
+fi
+echo "Cash:"$cash
+
+
